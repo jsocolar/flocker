1e49f2d8494fae2a96ba3bebd10f6ddefeba9511
Jacob Socolar <jacob.socolar@gmail.com>
2021-07-30 16:02:55 -0500

    bringing the summary method online

--- R/summary.flocker_fit.R
+++ R/summary.flocker_fit.R
@@ -1,3 +1,8 @@
+#' Summary method for a flocker_fit object
+#' @param flocker_fit A flocker_fit object produced by \code{flocker(..., visit_constant = F)}
+#' @return Print a summary output for the fit.
+#' @export
+
 summary.flocker_fit <- function(flocker_fit) {
   writeLines(paste(flocker_fit$summary, collapse = "\n"))
 }
