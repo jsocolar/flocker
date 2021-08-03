ec7019386a5643ab66ac81ff7120baf45f98afc1
Jacob Socolar <jacob.socolar@gmail.com>
2021-08-03 00:07:11 -0500

    trying to figure out how summary should play with r cmd check

--- R/summary.flocker_fit.R
+++ R/summary.flocker_fit.R
@@ -1,9 +1,20 @@
-#' Summary method for a flocker_fit object
-#' @param flocker_fit A flocker_fit object produced by \code{flocker(..., visit_constant = F)}
-#' @param ... Other potential arguments
+#' Summary method for a flocker_fit object.
+#' @param flocker_fit A flocker_fit object produced by \code{flocker(..., visit_constant = F)}.
+#' @param ... Other potential arguments.
 #' @method summary flocker_fit
 #' @export
 
 summary.flocker_fit <- function(flocker_fit) {
-  writeLines(paste(flocker_fit$summary, collapse = "\n"))
+  out <- flocker_fit$summary
+  class(out) <- "flocker_summary"
+}
+
+#' Print a summary for a fitted model represented by a \code{flocker_fit} object.
+#' @aliases print.flocker_summary
+#' @param flocker_fit An object of class \code{flocker_fit}.
+#' @param ... Other potential arguments passed to method \code{summary} of \code{flocker_fit}.
+#' @seealso \code{\link{summary.flocker_fit}}
+#' @export
+print.flocker_fit <- function(flocker_fit, ...) {
+  writeLines(paste(summary(flocker_fit, ...), collapse = "\n"))
 }
