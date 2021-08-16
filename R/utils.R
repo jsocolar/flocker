a5c8a820954c8a1bcbb3082e1770c31f48702f42
SimonCMills <smills2@sheffield.ac.uk>
2021-08-16 15:47:15 +0100

    vectorised utils functions

--- R/utils.R
+++ R/utils.R
@@ -2,22 +2,19 @@
 #' @param x logit-scale probability
 #' @return the logarithm of the inverse logit of x
 log_inv_logit <- function (x) {
-  if (x < 0.0) {
-    out <- x - log1p(exp(x))
-  } else {
-    out <- -log1p(exp(-x))
-  }
+  out <- ifelse(x < 0.0, 
+                x - log1p(exp(x)), 
+                -log1p(exp(-x)))
   return(out)
 }
+
 
 #' Numerically stable log one-minus inverse logit
 #' @param x logit-scale probability
 #' @return the logarithm of one minus the inverse logit of x
 log1m_inv_logit <- function (x) {
-  if (x > 0.0) {
-    out <- -x - log1p(exp(-x))
-  } else {
-    out <- -log1p(exp(x))
-  }
+  out <- ifelse(x > 0.0, 
+                -x - log1p(exp(-x)), 
+                -log1p(exp(x)))
   return(out)
 }
