ccdcb74befdfb47d15ccfeedac0d5d56c9e14628
SimonCMills <smills2@sheffield.ac.uk>
2021-08-17 10:19:16 +0100

    refactored example data simulation code

--- R/example_flocker_data.R
+++ R/example_flocker_data.R
@@ -13,33 +13,42 @@ example_flocker_data <- function(visit_constant = FALSE, seed = 123) {
   n_sp <- 30
   n_unit <- n_pt*n_sp
   n_visit <- 4
-  unit_covs <- data.frame(sc1 = stats::rnorm(n_unit), sc2 = stats::rnorm(n_unit),
-                          grp = sample(c(1:20), n_unit, replace = T),
-                          species = rep(paste0("sp_", 1:n_sp), n_pt))
-  logit_psi <- 0 + 1*unit_covs$sc1 - 1*unit_covs$sc2 + stats::rnorm(20)[unit_covs$grp]
-  true_Z <- stats::rbinom(n_unit, 1, boot::inv.logit(logit_psi))
-  obs <- matrix(0, nrow=n_unit, ncol=n_visit)
-  if (visit_constant) {
-    visit_covs = NULL
-    for (i in 1:n_unit) {
-      if (true_Z[i] == 1) {
-        obs[i, ] <- stats::rbinom(4, 1, .5)
-      }
+  
+  backbone <- expand.grid(id_species = factor(paste0("sp_", 1:n_sp)), 
+                          id_visit = 1:n_visit,
+                          id_point = 1:n_pt)
+  
+  unit_covs <- within(unique(backbone[c("id_species", "id_point")]), {
+    id_unit = 1:n_unit
+    sc1 = stats::rnorm(n_pt)[id_point]
+    sc2 = stats::rnorm(n_pt)[id_point]
+    grp = sample(c(1:20), n_unit, replace = T)
+    logit_psi = 0 + 1*sc1 - 1*sc2 + stats::rnorm(20)[grp]
+    true_Z = stats::rbinom(n_unit, 1, boot::inv.logit(logit_psi))
+  })
+  
+  df_full <- within(merge(backbone, unit_covs), {
+    id_point_visit <- interaction(id_point, id_visit)
+    vc1 = stats::rnorm(n_pt * n_visit)[id_point_visit]
+    vc2 = stats::rnorm(n_pt * n_visit)[id_point_visit]
+    logit_theta = -.5 + .5*sc1 + stats::rnorm(20)[grp] + .5*vc1 - 1*vc2
+    obs = if(visit_constant) {
+      stats::rbinom(n_unit * n_visit, true_Z, .5) 
+    } else {
+      stats::rbinom(n_unit * n_visit, true_Z, boot::inv.logit(logit_theta))
     }
-  } else {
-    visit_covs <- list(vc1 = matrix(stats::rnorm(n_unit*n_visit), nrow=n_unit), vc2 = matrix(stats::rnorm(n_unit*n_visit), nrow=n_unit))
-    logit_theta <- matrix(rep((-.5 + .5*unit_covs$sc1 + stats::rnorm(20)[unit_covs$grp]), 4), nrow = n_unit) +
-      .5*visit_covs$vc1 - 1*visit_covs$vc2
-    for (i in 1:n_unit) {
-      if (true_Z[i] == 1) {
-        for (j in 1:4) {
-          obs[i, j] <- stats::rbinom(1, 1, boot::inv.logit(logit_theta[i, j]))
-        }
-      }
-    }
-  }
-  example_data <- list(obs = obs, unit_covs = unit_covs, visit_covs = visit_covs)
-  example_data
+  })
+  
+  # format data for return
+  out <- list(obs = t(unstack(df_full[c("obs", "id_unit")], obs ~ id_unit)), 
+              unit_covs = unit_covs[c("sc1", "sc2", "grp", "id_species")], 
+              visit_covs = list(
+                vc1 = t(unstack(df_full[c("vc1", "id_unit")], vc1 ~ id_unit)), 
+                vc2 = t(unstack(df_full[c("vc2", "id_unit")], vc2 ~ id_unit))
+              ))
+  rownames(out$obs) <- NULL
+  rownames(out$visit_covs$vc1) <- NULL
+  rownames(out$visit_covs$vc2) <- NULL
+  return(out)
 }
-
 
