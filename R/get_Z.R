9e6c43c62c84118551960f8d5146357fe8d3f96f
Jacob <jacob@ag-clo-jss473adcornelledu.local>
2021-09-09 23:17:25 -0400

    building out get_Z to accept new data and optionally not condition directly on the observed detection history

--- R/get_Z.R
+++ R/get_Z.R
@@ -1,12 +1,28 @@
 #' Get posterior distribution of Z matrix
 #' @param flocker_fit A flocker_fit object
 #' @param n_iter The number of posterior iterations desired
+#' @param hist_condition Should the posterior distribution for Z directly 
+#'     condition on the observed detection history (`TRUE`) or not (`FALSE`)?
+#'     For example, at sites with at least one detection, the true occupancy 
+#'     state conditioned on the history is one with absolute certainty. Without 
+#'     directly conditioning on the history, the occupancy state is controlled 
+#'     by the posterior distribution for the occupancy probability psi. Of 
+#'     course even without conditioning directly on the detection history, we 
+#'     still condition indirectly on the observed history via the fitted value 
+#'     of psi, which itself depends on all of the observed detection histories.
+#' @param new_data Optional new data at which to predict the Z matrix. Generally
+#'     the output of `make_flocker_data`. Cannot be used if `hist_condition` is
+#'     set to `TRUE`.
+#' @param sample_new_levels If new_data is provided and contains random effect
+#'     levels not present in the original data, how should predictions be
+#'     handled? Passed directly to brms::prepare_predictions, which see. 
 #' @return The posterior Z matrix. Rows are iterations and columns
 #'     are closure-units. Values are samples from the posterior distribution
 #'     of occupancy probability.
 #' @export
 
-get_Z <- function (flocker_fit, n_iter = NULL) {
+get_Z <- function (flocker_fit, n_iter = NULL, hist_condition = TRUE, 
+                   new_data = NULL, sample_new_levels = "uncertainty") {
   if (!is_flocker_fit(flocker_fit)) {
     stop("flocker_fit must be an object of class `flocker_fit`")
   }
@@ -27,47 +43,69 @@ get_Z <- function (flocker_fit, n_iter = NULL) {
     stop("requested more iterations than contained in flocker_fit")
   }
   
+  if (is.null(new_data)) {
+    new_data <- flocker_fit$data
+  } else if (hist_condition) {
+    stop("cannot condition on Q if new_data is provided")
+  } else if (!("flocker_data" %in% class(new_data))) {
+    stop("new_data, if provided, must be a `flocker_data` object as produced by 
+         `make_flocker_data`")
+  } else {
+    new_data <- new_data$data
+  }
+  
   iter <- (1:n_iter)*floor(total_iter/n_iter)
   
-  lpo <- brms::posterior_linpred(flocker_fit, dpar = "occ", draw_ids = iter)
-  lpd <- brms::posterior_linpred(flocker_fit, dpar = "mu", draw_ids = iter)
-  
+  lpo <- brms::posterior_linpred(flocker_fit, dpar = "occ", draw_ids = iter, 
+                                 newdata = new_data, allow_new_levels = TRUE,
+                                 sample_new_levels = sample_new_levels)
+  lpd <- brms::posterior_linpred(flocker_fit, dpar = "mu", draw_ids = iter,
+                                 newdata = new_data, allow_new_levels = TRUE,
+                                 sample_new_levels = sample_new_levels)
+
   lik_type <- attributes(flocker_fit)$lik_type
   
   if (lik_type == "V") {
-    n_unit <- flocker_fit$data$n_unit[1]
-    Q <- as.integer(flocker_fit$data$Q[1:n_unit])
-    Z <- matrix(data = 1, nrow = length(iter), ncol = n_unit)
-    psi_all <- boot::inv.logit(lpo)
-    antitheta_all <- boot::inv.logit(-lpd)
-    
-    index_matrix <- flocker_fit$data[grep("^rep_index", names(flocker_fit$data))]
-    
-    for (i in 1:n_unit) {
-      if (Q[i] == 0L) {
-        psi <- psi_all[ , i]
-        antitheta <- antitheta_all[ , as.integer(index_matrix[1:flocker_data$data$n_rep[i], i])]
-        numerator <- psi * matrixStats::rowProds(antitheta)
-        denominator <- numerator + (1 - psi)
-        Z[, i] <- numerator/denominator
-      }
-    }
+    n_unit <- new_data$n_unit[1]
+    lpo <- lpo[, 1:n_unit]
   } else {
     n_unit <- nrow(flocker_fit$data)
-    Q <- as.integer(flocker_fit$data$n_suc > 0)
-    Z <- matrix(data = 1, nrow = length(iter), ncol = n_unit)
-    psi_all <- boot::inv.logit(lpo)
+  }
+  
+  psi_all <- boot::inv.logit(lpo)
+  Z <- matrix(data = 1, nrow = length(iter), ncol = n_unit)
+  
+  if (!hist_condition) {
+    Z <- psi_all
+  } else {
     antitheta_all <- boot::inv.logit(-lpd)
-    for (i in 1:n_unit) {
-      if (Q[i] == 0L) {
-        psi <- psi_all[, i]
-        antitheta <- antitheta_all[, i]
-        numerator <- psi * antitheta^flocker_fit$data$n_trial[i]
+    if (lik_type == "V") {
+      index_matrix <- new_data[grep("^rep_index", names(new_data))]
+      Q <- as.integer(flocker_fit$data$Q[1:n_unit])
+      for (i in 1:n_unit) {
+        if (Q[i] == 0L) {
+          psi <- psi_all[ , i]
+          antitheta <- antitheta_all[ , as.integer(index_matrix[1:flocker_data$data$n_rep[i], i])]
+          numerator <- psi * matrixStats::rowProds(antitheta)
           denominator <- numerator + (1 - psi)
           Z[, i] <- numerator/denominator
+        }
+      }
+    } else {
+      Q <- as.integer(flocker_fit$data$n_suc > 0)
+      Z <- matrix(data = 1, nrow = length(iter), ncol = n_unit)
+      for (i in 1:n_unit) {
+        if (Q[i] == 0L) {
+          psi <- psi_all[, i]
+          antitheta <- antitheta_all[, i]
+          numerator <- psi * antitheta^flocker_fit$data$n_trial[i]
+          denominator <- numerator + (1 - psi)
+          Z[, i] <- numerator/denominator
+        }
       }
     }
   }
+
   class(Z) <- c("postZ", "matrix")
   return(Z)
 }
