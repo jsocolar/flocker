6b365995ec71d1dcc7c6ceaed3fc71ea7eb29910
SimonCMills <smills2@sheffield.ac.uk>
2021-09-14 11:53:22 +0100

    updated following changes to example_flocker_data()

--- tests/testthat/test-make_flocker_data.R
+++ tests/testthat/test-make_flocker_data.R
@@ -9,17 +9,17 @@ test_that("make_flocker_data works correctly", {
   expect_equal(class(fd), c("list", "flocker_data"))
   expect_equal(names(fd), c("data", "max_rep", "type"))
   expect_equal(
-    sum(fd$data[1:3000, c("rep_index1", "rep_index2", 
+    sum(fd$data[1:900, c("rep_index1", "rep_index2", 
                           "rep_index3", "rep_index4")] - 
-          matrix(1:12000, ncol = 4)), 0)  
+          matrix(1:3600, ncol = 4)), 0)  
   expect_equal(names(fd$data), c("y", "uc1", "uc2", "grp", "species", "ec1", "ec2",
                                  "n_unit", "n_rep", "Q", "unit", "rep_index1", 
                                  "rep_index2", "rep_index3", "rep_index4"))
   expect_true(all(fd$data$occ %in% c(0,1,-99)))
   
-  event_covs[[1]] <- matrix(1:12000, ncol = 4)
+  event_covs[[1]] <- matrix(1:3600, ncol = 4)
   fd <- make_flocker_data(obs, unit_covs, event_covs)
-  expect_equal(fd$data$ec1, 1:12000)
+  expect_equal(fd$data$ec1, 1:3600)
   
   
   obs[2,4] <- NA
@@ -28,15 +28,15 @@ test_that("make_flocker_data works correctly", {
   expect_equal(class(fd), c("list", "flocker_data"))
   expect_equal(names(fd), c("data", "max_rep", "type"))
   expect_equal(
-    sum(fd$data[1:3000, c("rep_index1", "rep_index2", "rep_index3")] - 
-          matrix(1:9000, ncol = 3)), 0)
-  expect_equal(fd$data$rep_index4[1:3000], c(9001, -99, 9002:11999))
+    sum(fd$data[1:900, c("rep_index1", "rep_index2", "rep_index3")] - 
+          matrix(1:2700, ncol = 3)), 0)
+  expect_equal(fd$data$rep_index4[1:900], c(2701, -99, 2702:3599))
   expect_equal(names(fd$data), c("y", "uc1", "uc2", "grp", "species", "ec1", "ec2",
                                  "n_unit", "n_rep", "Q", "unit", "rep_index1", 
                                  "rep_index2", "rep_index3", "rep_index4"))
   expect_true(all(fd$flocker_data$occ %in% c(0,1,-99)))
-  expect_equal(fd$data$ec1[1:9000], 1:9000)
-  expect_equal(fd$data$ec1[9001:11999], c(9001, 9003:12000))
+  expect_equal(fd$data$ec1[1:2700], 1:2700)
+  expect_equal(fd$data$ec1[2701:3599], c(2701, 2703:3600))
   
   obs[,4] <- NA
   expect_error(fd <- make_flocker_data(obs),
