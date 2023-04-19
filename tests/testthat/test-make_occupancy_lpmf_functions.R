40cbd72f0cecc9ec64774f49727cbbb53eca8945
jsocolar <jacob.socolar@gmail.com>
2023-04-18 22:54:46 -0400

    unit tests for more lpmf-related functions

--- tests/testthat/test-make_occupancy_lpmf_functions.R
+++ tests/testthat/test-make_occupancy_lpmf_functions.R
@@ -68,3 +68,16 @@ test_that("make_occupancy_augmented_lpmf works correctly", {
   # Test with max_rep = 3.5 (invalid input)
   expect_error(make_occupancy_augmented_lpmf(3.5), "max_rep must be an integer greater than 1")
 })
+
+
+test_that("no-argument functions have correct return types", {
+  expect_is(make_occupancy_single_C_lpmf(), "character")
+  expect_is(make_emission_1(), "character")
+  expect_is(make_emission_1_C(), "character")
+  expect_is(make_emission_1_fp(), "character")
+  expect_is(make_emission_0_fp(), "character")
+  expect_is(make_colex_likelihoods(), "character")
+  expect_is(make_colex_fp_likelihoods(), "character")
+  expect_is(make_forward_colex(), "character")
+  expect_is(make_forward_colex_fp(), "character")
+})
