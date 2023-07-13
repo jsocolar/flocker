e2c627ad61c46fe8fde303908333958e04b6405d
jsocolar <jacob.socolar@gmail.com>
2023-07-13 18:12:43 -0400

    fixing failing tests

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -103,7 +103,7 @@ test_that("fdtl function returns expected dataframe", {
   expect_named(result, c("model_type", "data_output_type", "data_input_type"))
   
   # Check if the result has the correct number of rows (assuming 10 model types)
-  expect_equal(nrow(result), 10)
+  expect_equal(nrow(result), 7)
   
   # Check if the result has the correct number of columns
   expect_equal(ncol(result), 3)
@@ -113,13 +113,11 @@ test_that("fdtl function returns expected dataframe", {
   
   # Check if the data_output_type and data_input_type columns contain the expected values
   expected_data_input_types <- c(
-    "single", "single", "augmented", "multi", "multi", "multi", "multi",
-    "single", "multi", "multi"
+    "single", "single", "augmented", "multi", "multi", "multi", "multi"
   )
   
   expected_data_output_types <- c(
-    "single", "single_C", "augmented", "multi", "multi", "multi", "multi",
-    "single", "multi", "multi"
+    "single", "single_C", "augmented", "multi", "multi", "multi", "multi"
   )
   
   
