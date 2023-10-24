e5c2f97052a19009d6e38abaffc153bc3c232ff4
jsocolar <jacob.socolar@gmail.com>
2023-10-24 12:14:44 -0400

    progress

--- inst/cache_vignettes.R
+++ inst/cache_vignettes.R
@@ -1,22 +1,33 @@
 # This script creates .Rmd vignettes that include pre-computed R output so that
 # whent they get built on CI it's a lightweight operation.
 
-# This function should be run every time there is an update to the vignettes
+# This script should be run every time there is an update to the vignettes
 # or an update to the package for which the vignettes are a desirable
 # part of the test harness (but ideally the actual test harness should be 
 # sufficient for testing, and we shouldn't rely on vignettes).
+
+# Edits that touch only the text of the vignettes, and not the R computation,
+# can be made with extreme care by changing both the .Rmd.orig files and the
+# .Rmd files with computation cached.
 
 setwd(paste0(dirname(rstudioapi::getActiveDocumentContext()$path), "/.."))
 
 knitr::knit(
   "vignettes/augmented_models.Rmd.orig", 
   output = "vignettes/augmented_models.Rmd"
-  )
+)
+
 knitr::knit(
   "vignettes/flocker_tutorial.Rmd.orig", 
   output = "vignettes/flocker_tutorial.Rmd"
 )
+
 knitr::knit(
   "vignettes/nonlinear_models.Rmd.orig", 
   output = "vignettes/nonlinear_models.Rmd"
+)
+
+knitr::knit(
+  "vignettes/sbc.Rmd.orig", 
+  output = "vignettes/sbc.Rmd"
 )
