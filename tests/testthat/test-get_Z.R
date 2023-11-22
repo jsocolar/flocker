eb5b9b41fa69b4f58e136c53f69af049691bd302
jsocolar <jacob.socolar@gmail.com>
2023-11-21 21:21:50 -0500

    more tests

--- tests/testthat/test-get_Z.R
+++ tests/testthat/test-get_Z.R
@@ -105,3 +105,90 @@ test_that("new_data works as expected", {
 #  expect_silent(get_Z(example_flocker_model_single, history_condition = FALSE, new_data = fd1$unit_covs))
   expect_error(get_Z(example_flocker_model_single, history_condition = TRUE, new_data = fd1$unit_covs))
 })
+
+
+
+
+
+# Test 1: Test that the function throws an error for incorrect input types
+test_that("incorrect types cause an error", {
+  expect_error(forward_sim("init", c(0.5, NA), c(0.5, NA)))
+  expect_error(forward_sim(0.5, "colo", c(0.5, NA)))
+  expect_error(forward_sim(0.5, c(0.5, NA), "ex"))
+})
+
+# Test 2: Test that the function throws an error for negative probabilities
+test_that("negative probabilities cause an error", {
+  expect_error(forward_sim(-0.1, c(0.5, NA), c(0.5, NA)))
+  expect_error(forward_sim(0.5, c(-0.5, NA), c(0.5, NA)))
+  expect_error(forward_sim(0.5, c(0.5, NA), c(-0.5, NA)))
+})
+
+# Test 3: Test that the function returns correct results for valid inputs
+test_that("correct inputs produce correct outputs", {
+  # Simple case with no NA values and no sampling
+  expect_equal(forward_sim(0.5, c(0.5, 0.5), c(0.5, 0.5)), c(0.5, 0.5))
+  # Simple case with sampling, the result is stochastic, so we can't test exact values, just types and lengths
+  result <- forward_sim(0.5, c(0.5, 0.5), c(0.5, 0.5), sample = TRUE)
+  expect_type(result, "integer")
+  expect_length(result, 2)
+})
+
+# Test 4: Test edge cases
+test_that("edge cases are handled", {
+  # Case with all NA values should return NA
+  expect_error(forward_sim(NA, c(NA, NA), c(NA, NA)))
+  # Case with empty vectors
+  expect_error(forward_sim(0.5, numeric(0), numeric(0)))
+})
+
+
+
+
+
+# Test for forward_backward_algorithm function
+test_that("forward_backward_algorithm returns correct results", {
+  el0 <- c(0.1, 0.2, 0.3)
+  el1 <- c(0.9, 0.8, 0.7)
+  init <- 0.5
+  colo <- c(0.6, 0.7, 0.8)
+  ex <- c(0.4, 0.3, 0.2)
+  result <- forward_backward_algorithm(el0, el1, init, colo, ex)
+  expect_type(result, "double")
+  expect_length(result, length(el0))
+})
+
+# Test for forward_backward_sampling function
+test_that("forward_backward_sampling returns correct results", {
+  el0 <- c(0.1, 0.2, 0.3)
+  el1 <- c(0.9, 0.8, 0.7)
+  init <- 0.5
+  colo <- c(0.6, 0.7, 0.8)
+  ex <- c(0.4, 0.3, 0.2)
+  result <- forward_backward_sampling(el0, el1, init, colo, ex)
+  expect_type(result, "double")
+  expect_length(result, length(el0))
+})
+
+# Test for forward_algorithm function
+test_that("forward_algorithm returns correct results", {
+  el0 <- c(0.1, 0.2, 0.3)
+  el1 <- c(0.9, 0.8, 0.7)
+  init <- 0.5
+  colo <- c(0.6, 0.7, 0.8)
+  ex <- c(0.4, 0.3, 0.2)
+  result <- forward_algorithm(el0, el1, init, colo, ex)
+  expect_type(result, "double")
+  expect_equal(dim(result), c(length(el0), 2))
+})
+
+# Test for backward_algorithm function
+test_that("backward_algorithm returns correct results", {
+  el0 <- c(0.1, 0.2, 0.3)
+  el1 <- c(0.9, 0.8, 0.7)
+  colo <- c(0.6, 0.7, 0.8)
+  ex <- c(0.4, 0.3, 0.2)
+  result <- backward_algorithm(el0, el1, colo, ex)
+  expect_type(result, "double")
+  expect_equal(dim(result), c(length(el0), 2))
+})
