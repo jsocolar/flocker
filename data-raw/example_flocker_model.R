b414c23c5618c3fe193db1bab255b56d5323fac8
jsocolar <jacob.socolar@gmail.com>
2023-12-06 13:24:02 -0500

    refactoring to avoid storing large flockerfit model objects

--- data-raw/example_flocker_model.R
+++ data-raw/example_flocker_model.R
@@ -3,151 +3,21 @@
 set.seed(1)
 
 #### single-season model ####
+fd <- simulate_flocker_data(n_pt = 20, n_sp = 5)
 mfd <- make_flocker_data(
-  example_flocker_data$obs, 
-  example_flocker_data$unit_covs, 
-  example_flocker_data$event_covs
+  fd$obs, 
+  fd$unit_covs,
+  fd$event_covs
 )
 example_flocker_model_single <- flock(
   f_occ = ~ 0 + Intercept + uc1 + (1 + uc1 | species),
   f_det = ~ 0 + Intercept + uc1 + ec1 + (1 + uc1 + ec1 | species),
   flocker_data = mfd,
-  cores = 4,
-  iter = 1000,
-  warmup = 996,
-  save_warmup = FALSE
-)
-
-#### single-season rep-constant model ####
-mfd <- make_flocker_data(
-  example_flocker_data$obs, 
-  example_flocker_data$unit_covs
-)
-example_flocker_model_single_C <- flock(
-  f_occ = ~ 0 + Intercept + uc1 + (1 + uc1 | species),
-  f_det = ~ 0 + Intercept + uc1 + (1 + uc1 | species),
-  flocker_data = mfd,
-  cores = 4,
-  iter = 1000,
-  warmup = 996,
-  save_warmup = FALSE
-)
-
-#### data augmented model ####
-fd <- simulate_flocker_data(augmented = TRUE,
-                            n_sp = 50)
-mfd <- make_flocker_data(
-  fd$obs, 
-  fd$unit_covs, 
-  fd$event_covs,
-  type = "augmented",
-  n_aug = 100
-)
-example_flocker_model_aug <- flock(
-  f_occ = ~ 0 + Intercept + (1 | ff_species),
-  f_det = ~ 0 + Intercept + uc1 + ec1 + (1 + uc1 + ec1 | ff_species),
-  flocker_data = mfd,
-  augmented = TRUE,
-  cores = 4,
-  iter = 1000,
-  warmup = 996,
-  save_warmup = FALSE
-  )
-
-#### multiseason colex with explicit inits ####
-fd <- simulate_flocker_data(
-  n_pt = 200, n_sp = 1, n_season = 6, 
-  multiseason = "colex", multi_init = "explicit",
-  ragged_rep = TRUE, missing_seasons = TRUE
-  )
-mfd <- make_flocker_data(
-  fd$obs, fd$unit_covs, fd$event_covs, type = "multi"
-)
-example_flocker_model_multi_colex_ex <- flock(
-  f_occ = ~ 0 + Intercept + uc1,
-  f_det = ~ 0 + Intercept + uc1 + ec1,
-  f_col = ~ 0 + Intercept + uc1,
-  f_ex = ~ 0 + Intercept + uc1,
-  flocker_data = mfd,
-  multiseason = "colex",
-  multi_init = "explicit",
-  cores = 4,
-  iter = 1000,
-  warmup = 996,
-  save_warmup = FALSE
-)
-
-#### multiseason colex with equilibrium inits ####
-fd <- simulate_flocker_data(
-  n_pt = 200, n_sp = 1, n_season = 6, 
-  multiseason = "colex", multi_init = "equilibrium",
-  ragged_rep = TRUE, missing_seasons = TRUE
-)
-mfd <- make_flocker_data(
-  fd$obs, fd$unit_covs, fd$event_covs, type = "multi"
-)
-example_flocker_model_multi_colex_eq <- flock(
-  f_det = ~ 0 + Intercept + uc1 + ec1,
-  f_col = ~ 0 + Intercept + uc1,
-  f_ex = ~ 0 + Intercept + uc1,
-  flocker_data = mfd,
-  multiseason = "colex",
-  multi_init = "equilibrium",
-  cores = 4,
-  iter = 1000,
-  warmup = 996,
-  save_warmup = FALSE
-)
-
-#### multiseason autologistic with explicit inits ####
-fd <- simulate_flocker_data(
-  n_pt = 200, n_sp = 1, n_season = 6, 
-  multiseason = "autologistic", multi_init = "explicit",
-  ragged_rep = TRUE, missing_seasons = TRUE
-)
-mfd <- make_flocker_data(
-  fd$obs, fd$unit_covs, fd$event_covs, type = "multi"
-)
-example_flocker_model_multi_auto_ex <- flock(
-  f_occ = ~ 0 + Intercept + uc1,
-  f_det = ~ 0 + Intercept + uc1 + ec1,
-  f_col = ~ 0 + Intercept + uc1,
-  f_auto = ~ 0 + Intercept + uc1,
-  flocker_data = mfd,
-  multiseason = "autologistic",
-  multi_init = "explicit",
-  cores = 4,
-  iter = 1000,
-  warmup = 996,
-  save_warmup = FALSE
-)
-
-#### multiseason autologistic with equilibrium inits ####
-fd <- simulate_flocker_data(
-  n_pt = 200, n_sp = 1, n_season = 6, 
-  multiseason = "autologistic", multi_init = "equilibrium",
-  ragged_rep = TRUE, missing_seasons = TRUE
-)
-mfd <- make_flocker_data(
-  fd$obs, fd$unit_covs, fd$event_covs, type = "multi"
-)
-example_flocker_model_multi_auto_eq <- flock(
-  f_det = ~ 0 + Intercept + uc1 + ec1,
-  f_col = ~ 0 + Intercept + uc1,
-  f_auto = ~ 0 + Intercept + uc1,
-  flocker_data = mfd,
-  multiseason = "autologistic",
-  multi_init = "equilibrium",
-  cores = 4,
+  cores = 2,
+  chains = 2,
   iter = 1000,
   warmup = 996,
   save_warmup = FALSE
 )
 
 usethis::use_data(example_flocker_model_single, overwrite = TRUE)
-usethis::use_data(example_flocker_model_single_C, overwrite = TRUE)
-usethis::use_data(example_flocker_model_aug, overwrite = TRUE)
-usethis::use_data(example_flocker_model_multi_colex_ex, overwrite = TRUE)
-usethis::use_data(example_flocker_model_multi_colex_eq, overwrite = TRUE)
-usethis::use_data(example_flocker_model_multi_auto_ex, overwrite = TRUE)
-usethis::use_data(example_flocker_model_multi_auto_eq, overwrite = TRUE)
