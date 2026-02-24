a7ae124001318adf5690220efab1292fbc688077
jsocolar <jacob.socolar@gmail.com>
2026-02-24 13:02:02 -0500

    better vignette building

--- inst/cache_vignettes.R
+++ inst/cache_vignettes.R
@@ -1,37 +1,58 @@
-# This script creates .Rmd vignettes that include pre-computed R output so that
-# whent they get built on CI it's a lightweight operation.
+if (!file.exists("DESCRIPTION")) {
+  stop("Run this script from the package root (where DESCRIPTION lives).")
+}
 
-# This script should be run every time there is an update to the vignettes
-# or an update to the package for which the vignettes are a desirable
-# part of the test harness (but ideally the actual test harness should be 
-# sufficient for testing, and we shouldn't rely on vignettes).
+suppressPackageStartupMessages({
+  library(withr)
+  library(knitr)
+})
 
-# Edits that touch only the text of the vignettes, and not the R computation,
-# can be made with extreme care by changing both the .Rmd.orig files and the
-# .Rmd files with computation cached.
+find_pkg_root <- function() {
+  path <- normalizePath(getwd(), winslash = "/", mustWork = TRUE)
+  repeat {
+    if (file.exists(file.path(path, "DESCRIPTION"))) return(path)
+    parent <- normalizePath(file.path(path, ".."), winslash = "/", mustWork = TRUE)
+    if (identical(parent, path)) stop("Could not find package root (no DESCRIPTION found).")
+    path <- parent
+  }
+}
 
-old_wd <- getwd()
+pkg_root <- find_pkg_root()
 
-setwd(paste0(dirname(rstudioapi::getActiveDocumentContext()$path), "/.."))
-
-knitr::knit(
-  "vignettes/augmented_models.Rmd.orig", 
-  output = "vignettes/augmented_models.Rmd"
+vigs <- list(
+  c("vignettes/augmented_models.Rmd.orig", "vignettes/augmented_models.Rmd"),
+  c("vignettes/flocker_tutorial.Rmd.orig", "vignettes/flocker_tutorial.Rmd"),
+  c("vignettes/nonlinear_models.Rmd.orig", "vignettes/nonlinear_models.Rmd.orig"), # (if desired)
+  c("vignettes/nonlinear_models.Rmd.orig", "vignettes/nonlinear_models.Rmd"),
+  c("vignettes/articles/sbc.Rmd.orig", "vignettes/articles/sbc.Rmd")
 )
 
-knitr::knit(
-  "vignettes/flocker_tutorial.Rmd.orig", 
-  output = "vignettes/flocker_tutorial.Rmd"
-)
+with_dir(pkg_root, {
+  # Ensure relative paths in chunks resolve from package root
+  old_root <- knitr::opts_knit$get("root.dir")
+  on.exit(knitr::opts_knit$set(root.dir = old_root), add = TRUE)
+  knitr::opts_knit$set(root.dir = pkg_root)
+  
+  knit_one <- function(infile, outfile, fig_path = NULL) {
+    dir.create(dirname(outfile), recursive = TRUE, showWarnings = FALSE)
+    
+    if (!is.null(fig_path)) {
+      dir.create(fig_path, recursive = TRUE, showWarnings = FALSE)
+      old_fig <- knitr::opts_chunk$get("fig.path")
+      on.exit(knitr::opts_chunk$set(fig.path = old_fig), add = TRUE)
+      knitr::opts_chunk$set(fig.path = paste0(fig_path, "/"))
+    }
+    
+    message("Knitting ", infile, " -> ", outfile)
+    knitr::knit(infile, output = outfile, envir = globalenv())
+  }
+  
+  knit_one(vigs[[1]][1], vigs[[1]][2])
+  knit_one(vigs[[2]][1], vigs[[2]][2])
+  knit_one(vigs[[4]][1], vigs[[4]][2])
+  
+  # SBC: force figures into man/figures/sbc_vignette
+  knit_one(vigs[[5]][1], vigs[[5]][2], fig_path = "man/figures/sbc_vignette")
+})
 
-knitr::knit(
-  "vignettes/nonlinear_models.Rmd.orig", 
-  output = "vignettes/nonlinear_models.Rmd"
-)
-
-knitr::knit(
-  "vignettes/sbc.Rmd.orig", 
-  output = "vignettes/sbc.Rmd"
-)
-
-setwd(old_wd)
+message("Done.")
